77ff02f31d1e0a67d4e3a7ad5a97d73f00771ba9
vadim.nichiporuk <vadzim.nichyparuk@gcore.lu>
2019-03-26 21:04:36 +0300

    schema relations changes

--- app/models/schedule.py
+++ app/models/schedule.py
@@ -6,7 +6,7 @@ class PeriodicityTypeModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
 
-    db.relationship('ScheduleModel', backref='type', lazy='dynamic')
+    schedule = db.relationship('ScheduleModel', backref='type', lazy='dynamic')
 
     def __str__(self):
         return self.name
